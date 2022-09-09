72e5c0e5730ba7905840c49852ecace3a28f63b9
_crhis_ <crhisnokia@live.com>
2022-09-08 22:15:35 -0300

    "fix bug merge"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 8866f4d6b3dc8a84a8d6faaae49b9d25ed42b488
 import { app } from "./app"
 import { bandRouter } from "./routes/bandRouter";
 import { userRouter } from './routes/userRouter';
@@ -14,7 +10,3 @@ app.use("/band", bandRouter)
 
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 8866f4d6b3dc8a84a8d6faaae49b9d25ed42b488
